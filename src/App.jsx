fe613f2746fb0dbbc9f2220eab02def05d10870f
artiq75 <artiq75@gmail.com>
2022-03-12 11:34:00 +0100

    Add global navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
 
 export function App() {
   return (
-    <div className="App">
-      <Outlet />
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Where in the world?</h1>
+      <h3>Dark mode</h3>
+    </div>
+  )
+}
